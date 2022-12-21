5e3bb21c4e032fd46de9b5774e5ba37023837392
Lil Hoon <47732431+LeaDerBone@users.noreply.github.com>
2022-12-21 21:46:12 +0900

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'IShowSpeed',
+      job: 'Youtuber',
+      image:
+        'https://editors.dexerto.com/wp-content/uploads/2022/10/07/iShowSpeed-PC-hacked-YouTube-Stream.jpg',
+      text:
+        "Christiano Ronaldo Sewey!",
+    },
+    {
+      id: 2,
+      name: 'Yamato',
+      job: 'Baseball Player',
+      image:
+        'https://static.zerochan.net/Yamato.%28ONE.PIECE%29.full.3393579.jpg',
+      text:
+        'Yamato is GOAT!',
+    },
+    {
+      id: 3,
+      name: 'Lionel Messi',
+      job: 'Soccer Player',
+      image:
+        'https://media.cnn.com/api/v1/images/stellar/prod/221218184732-messi-wc-trophy.jpg',
+      text:
+        'Finally I won the World Cup. so happy now',
+    },
+    {
+      id: 4,
+      name: 'Nikocado Avocado',
+      job: 'youtuber',
+      image:
+        'https://m.media-amazon.com/images/M/MV5BOWQ2M2Y4NGYtYjVjNS00OTVlLWFhMGMtZTQ5MTE3NGQxMjJmXkEyXkFqcGdeQXVyMTU3MTk5NTg2._V1_.jpg',
+      text:
+        "Peasants! It's Your Fault!"
+      }
+  ];
+  
+  export default reviews;
